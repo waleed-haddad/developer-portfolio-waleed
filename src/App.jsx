72a0f65fe6d173eb32d48a58dd347805defbcafc
Waleed Haddad <109683271+waleed-haddad@users.noreply.github.com>
2024-07-30 19:38:36 -0400

    Create introductory profile section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar"
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container mx-auto px-8">
         <Navbar />
+        <Profile />
       </div>
     </div>
   );
